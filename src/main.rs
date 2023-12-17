22d5667c9c2c645fad9cd5e1676c43ced34cd46a
Pasha Fistanto <pasha.fistanto@gmail.com>
2023-12-17 21:35:47 +0100

    Rename project

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use clap::Parser;
-use pngme::args::Cli;
-use pngme::Result;
-use pngme::commands::*;
+use png_secret::args::Cli;
+use png_secret::Result;
+use png_secret::commands::*;
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
     match cli.command {
-        pngme::args::Command::Encode(args) => encode_handler(args),
-        pngme::args::Command::Decode(args) => decode_handler(args),
-        pngme::args::Command::Remove(args) => remove_handler(args),
-        pngme::args::Command::Print(args) => print_handler(args),
+        png_secret::args::Command::Encode(args) => encode_handler(args),
+        png_secret::args::Command::Decode(args) => decode_handler(args),
+        png_secret::args::Command::Remove(args) => remove_handler(args),
+        png_secret::args::Command::Print(args) => print_handler(args),
     }
 }
 
